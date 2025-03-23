831ac15246f8657e5db8a57c46499cbdff80f63d
RoyalJonesGo <152839662+RoyalJonesGo@users.noreply.github.com>
2025-03-23 23:34:34 +0800

    Create DefaultEmailNotifier.java

---
+++ DefaultEmailNotifier.java
@@ -0,0 +1,6 @@
+public class DefaultEmailNotifier implements EmailNotifier{
+    @Override
+    public void sendEmailNotification(String email){
+        System.out.println("Email notification sent to: " + email);
+    }
+}
