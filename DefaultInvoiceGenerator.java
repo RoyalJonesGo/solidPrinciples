ff9767c58e1baa63e8370774fd702168c76b5add
RoyalJonesGo <152839662+RoyalJonesGo@users.noreply.github.com>
2025-03-23 23:33:56 +0800

    Create DefaultInvoiceGenerator.java

---
+++ DefaultInvoiceGenerator.java
@@ -0,0 +1,6 @@
+public class DefaultInvoiceGenerator implements InvoiceGenerator{
+    @Override
+    public void generateInvoice(String fileName){
+        System.out.println("Invoice generated: " + fileName);
+    }
+}
